4f1eace0382c91c8456f971ed589e904735c042a
Debabrata Mandal <bapidebabrata@gmail.com>
2022-08-16 14:30:14 +0530

    Define schema for medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,11 @@ CREATE TABLE patients(
     name text NOT NULL,
     date_of_birth date NOT NULL
 );
+
+
+CREATE TABLE medical_histories(
+    id SERIAL PRIMARY KEY,
+    patient_id integer NOT NULL REFERENCES patients(id),
+    admitted_at timestamp NOT NULL,
+    status varchar(50) NOT NULL
+);
